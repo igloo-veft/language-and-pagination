e1700bd3940837e32fe59567e2090e4c6ea4ca47
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-09-26 16:11:40 +0000

    adding summary

--- Models/Paging.cs
+++ Models/Paging.cs
@@ -2,9 +2,24 @@ namespace CoursesAPI.Models
 {
     public class Paging
     {
+        /// <summary>
+		/// The number of pages.
+		/// </summary>
         public int PageCount { get; set; }
+
+        /// <summary>
+		/// The number items in each page.
+		/// </summary>
         public int PageSize { get; set; }
+
+        /// <summary>
+		/// 1-based index of the current page being returned.
+		/// </summary>
         public int PageNumber { get; set; }
+
+        /// <summary>
+		/// Total number of items in the collection.
+		/// </summary>
         public int TotalNumberOfItems { get; set; }
     }
 }
