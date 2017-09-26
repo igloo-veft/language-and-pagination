0991fdea52823f84652a06e57503ee4625082194
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-09-26 20:47:40 +0000

    database migration files

--- API/Migrations/20170926204446_EnglishCourseNames.Designer.cs
+++ API/Migrations/20170926204446_EnglishCourseNames.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace API.Migrations
 {
     [DbContext(typeof(AppDataContext))]
-    partial class AppDataContextModelSnapshot : ModelSnapshot
+    [Migration("20170926204446_EnglishCourseNames")]
+    partial class EnglishCourseNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
